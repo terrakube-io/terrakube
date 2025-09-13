197152220a5676da0944419fd950ce5cdcc512c8
Alfredo España <alfespa17@gmail.com>
2025-09-13 09:01:56 -0600

    code refactor to replace deprecated methods

--- api/src/main/java/io/terrakube/api/plugin/token/team/TeamTokenService.java
+++ api/src/main/java/io/terrakube/api/plugin/token/team/TeamTokenService.java
@@ -61,33 +61,33 @@ public class TeamTokenService {
                 log.info("Team token will expire: {}", expiration);
 
                 jws = Jwts.builder()
-                        .setIssuer(ISSUER)
-                        .setSubject(String.format("%s (Team Token)", groupName))
-                        .setAudience(ISSUER)
-                        .setId(groupToken.getId().toString())
+                        .issuer(ISSUER)
+                        .subject(String.format("%s (Team Token)", groupName))
+                        .audience().add(ISSUER).and()
+                        .id(groupToken.getId().toString())
                         .claim("email", ownerEmail)
                         .claim("description", description)
                         .claim("email_verified", true)
                         .claim("name", String.format("%s (Token)", groupName))
                         .claim("groups", groupArray)
-                        .setIssuedAt(Date.from(Instant.now()))
-                        .setExpiration(expiration)
+                        .issuedAt(Date.from(Instant.now()))
+                        .expiration(expiration)
                         .signWith(key)
                         .compact();
             } else {
                 log.info("Team token will not expire:");
 
                 jws = Jwts.builder()
-                        .setIssuer(ISSUER)
-                        .setSubject(String.format("%s (Team Token)", groupName))
-                        .setAudience(ISSUER)
-                        .setId(groupToken.getId().toString())
+                        .issuer(ISSUER)
+                        .subject(String.format("%s (Team Token)", groupName))
+                        .audience().add(ISSUER).and()
+                        .id(groupToken.getId().toString())
                         .claim("email", ownerEmail)
                         .claim("description", description)
                         .claim("email_verified", true)
                         .claim("name", String.format("%s (Token)", groupName))
                         .claim("groups", groupArray)
-                        .setIssuedAt(Date.from(Instant.now()))
+                        .issuedAt(Date.from(Instant.now()))
                         .signWith(key)
                         .compact();
             }
