992d072d4bb79bf98f70239fc9869718bf68fac0
PK <212359285+capita-user@users.noreply.github.com>
2025-10-06 19:49:33 +0100

    Fix postgres AWS IAM token expiry issue (#2580)

--- api/src/main/java/io/terrakube/api/plugin/datasource/PostgresAwsIamAuthDataSource.java
+++ api/src/main/java/io/terrakube/api/plugin/datasource/PostgresAwsIamAuthDataSource.java
@@ -11,38 +11,52 @@ import java.sql.SQLException;
 
 @Slf4j
 public class PostgresAwsIamAuthDataSource extends PGSimpleDataSource {
-    public void setRegion(String region) {
-        this.awsRegion = Region.of(region);
-    }
 
     private Region awsRegion;
     private String authToken;
     private long lastTokenRefresh = 0;
-    private static final long TOKEN_EXPIRY_TIME = 10L * 60L * 1000L;
+    private static final long TOKEN_EXPIRY_TIME = 5L * 60L * 1000L;
+
+    public void setRegion(String region) {
+        this.awsRegion = Region.of(region);
+    }
 
     @Override
     public Connection getConnection(String user, String password) throws SQLException {
-        // If the token is older than 10 minutes, refresh it
+        refreshAuthTokenIfNeeded();
+
+        try {
+            return super.getConnection(user, authToken);
+        } catch (SQLException e) {
+            log.warn("Initial connection attempt failed, refreshing token and retrying...", e);
+            refreshAuthToken(); // force refresh
+            return super.getConnection(user, authToken);
+        }
+    }
+
+    private void refreshAuthTokenIfNeeded() {
         long currentTime = System.currentTimeMillis();
         if (currentTime - lastTokenRefresh > TOKEN_EXPIRY_TIME) {
-            lastTokenRefresh = currentTime;
-
-            log.debug("Refreshing postgres password using AWS SDK");
-
-            RdsUtilities rdsUtilities = RdsUtilities.builder()
-                    .region(awsRegion)
-                    .credentialsProvider(DefaultCredentialsProvider.create())
-                    .build();
-
-            authToken = rdsUtilities.generateAuthenticationToken(r -> r
-                    .hostname(getServerNames()[0])
-                    .port(getPortNumbers()[0])
-                    .username(getUser())
-                    .region(awsRegion)
-                    .build()
-            );
+            refreshAuthToken();
         }
+    }
 
-        return super.getConnection(user, authToken);
+    private void refreshAuthToken() {
+        log.debug("Refreshing Postgres IAM token using AWS SDK");
+
+        RdsUtilities rdsUtilities = RdsUtilities.builder()
+                .region(awsRegion)
+                .credentialsProvider(DefaultCredentialsProvider.create())
+                .build();
+
+        authToken = rdsUtilities.generateAuthenticationToken(r -> r
+                .hostname(getServerNames()[0])
+                .port(getPortNumbers()[0])
+                .username(getUser())
+                .region(awsRegion)
+                .build()
+        );
+
+        lastTokenRefresh = System.currentTimeMillis();
     }
 }
