cc6777c1d64ec64e2ae87ae38ef1b773159e4810
Bittrance <bittrance@gmail.com>
2025-11-22 13:42:49 +0100

    Executor preparation errors for reporting to api
    This commit only prepares the reporting. Updating the api to receive reports
    of early failures comes next.

---
+++ executor/src/main/java/io/terrakube/executor/service/workspace/WorkspaceException.java
@@ -0,0 +1,9 @@
+package io.terrakube.executor.service.workspace;
+
+public class WorkspaceException extends Exception {
+
+    public WorkspaceException(Exception e) {
+        super(e);
+    }
+
+}
