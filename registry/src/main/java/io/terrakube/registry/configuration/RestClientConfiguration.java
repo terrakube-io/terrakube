d33cac4926d63476d9d43022b2367a00518155f4
Alfredo España <alfespa17@gmail.com>
2026-02-17 08:05:55 -0600

    feat: introduce RestClient configuration for registry module
    - Added `RestClientConfiguration` class with a `RestClient` bean to support external HTTP client needs.

---
+++ registry/src/main/java/io/terrakube/registry/configuration/RestClientConfiguration.java
@@ -0,0 +1,14 @@
+package io.terrakube.registry.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestClient;
+
+@Configuration
+public class RestClientConfiguration {
+
+    @Bean
+    public RestClient restClient() {
+        return RestClient.create();
+    }
+}
