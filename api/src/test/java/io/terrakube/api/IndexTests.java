b641377dd3889819ebb3fc7bb67bd9f5100ca6a5
Alfredo España <alfespa17@gmail.com>
2026-03-19 14:55:38 -0600

    feat: mock DownloadReleasesService in IndexTests

--- api/src/test/java/io/terrakube/api/IndexTests.java
+++ api/src/test/java/io/terrakube/api/IndexTests.java
@@ -1,19 +1,46 @@
 package io.terrakube.api;
 
+import io.terrakube.api.plugin.json.DownloadReleasesService;
+import org.apache.commons.io.FileUtils;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.MockitoAnnotations;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.HttpStatus;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.charset.Charset;
 
 import static io.restassured.RestAssured.given;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.doAnswer;
 import static org.mockito.Mockito.when;
 
 public class IndexTests extends ServerApplicationTests {
 
+    @MockBean
+    DownloadReleasesService downloadReleasesService;
+
     @BeforeEach
-    public void setup() {
+    public void setup() throws IOException {
         MockitoAnnotations.openMocks(this);
         when(redisTemplate.opsForValue()).thenReturn(valueOperations);
+
+        doAnswer(invocation -> {
+            File file = invocation.getArgument(1);
+            FileUtils.writeStringToFile(file, "{\"mock\": \"data\"}", Charset.defaultCharset());
+            return null;
+        }).when(downloadReleasesService).downloadReleasesToFile(anyString(), any(File.class));
+
+        doAnswer(invocation -> {
+            File file = invocation.getArgument(1);
+            FileUtils.writeStringToFile(file, "{\"mock\": \"data\"}", Charset.defaultCharset());
+            return null;
+        }).when(downloadReleasesService).downloadReleasesToFile(anyString(), any(File.class), anyString());
     }
 
     @Test
@@ -24,8 +51,8 @@ public class IndexTests extends ServerApplicationTests {
                 .get("/terraform/index.json")
                 .then()
                 .assertThat()
-                //.log() dont show terraform response it is a lot of data
-                //.all()
+                .log()
+                .all()
                 .statusCode(HttpStatus.OK.value());
     }
 
@@ -37,8 +64,8 @@ public class IndexTests extends ServerApplicationTests {
                 .get("/tofu/index.json")
                 .then()
                 .assertThat()
-                //.log() dont show terraform response it is a lot of data
-                //.all()
+                .log()
+                .all()
                 .statusCode(HttpStatus.OK.value());
     }
 }
