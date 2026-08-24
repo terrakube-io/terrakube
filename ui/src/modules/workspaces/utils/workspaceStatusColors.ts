93ff783d99cf8efbe5550959c3169c8d1461efcd
Jake Groves <mrjakegroves@googlemail.com>
2026-08-24 23:35:20 +0100

    feat(ui): add pending status filter to workspace list

--- ui/src/modules/workspaces/utils/workspaceStatusColors.ts
+++ ui/src/modules/workspaces/utils/workspaceStatusColors.ts
@@ -4,6 +4,7 @@ export const statusColors: Record<string, string> = {
   [JobStatus.Completed]: "#2eb039",
   [JobStatus.Running]: "#108ee9",
   [JobStatus.Queue]: "#8c8c8c",
+  [JobStatus.Pending]: "#8c8c8c",
   [JobStatus.WaitingApproval]: "#fa8f37",
   [JobStatus.Rejected]: "#FB0136",
   [JobStatus.Failed]: "#FB0136",
