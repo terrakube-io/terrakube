f5c719bcfe7477c620911ef58c57d7308e4a7cfd
Daniil Kivenko <kivenkodaniil@gmail.com>
2026-09-06 22:13:06 +0100

    fix(ui): handle invalid workspace source URLs

--- ui/src/modules/workspaces/utils/getVcsNameFromUrl.ts
+++ ui/src/modules/workspaces/utils/getVcsNameFromUrl.ts
@@ -3,7 +3,9 @@ import formatSshUrl from "./formatSshUrl";
 export default function (normalizedUrl: string): string {
   // Let's just be safe in case the wrong url was passed
   const fixedUrl = formatSshUrl(normalizedUrl);
-  const urlObj = new URL(fixedUrl);
-  const pathname = urlObj.pathname;
-  return pathname.substring(1);
+  try {
+    return new URL(fixedUrl).pathname.substring(1);
+  } catch {
+    return normalizedUrl;
+  }
 }
